ac9b4047b203d8074c322b1380d6ce49992d7100
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-13 11:27:45 -0500

    le pregunte  aedizon como hacia para sin recargar  colocal la etiqueta menu

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './style.css'
 
+import imgUrl from '../public/vite.svg'
+document.getElementById('vite.svg').src = imgUrl;
+const main = document.querySelector('main');
+window.addEventListener('backgorun', (e) => {
+  console.log(location.bash.slide(1));
+});
+const llamado = async (exit) => {
+  console.log("llamado");
+  const precicion = await fetch('ruta')
+}
+console.log(llamado);
