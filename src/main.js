ff6bf2f8011a0e4055c8d031cfb4e0d2dc77cc04
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-13 12:02:11 -0500

    intentando crear un crud y cambiar en una simple ladin page

--- src/main.js
+++ src/main.js
@@ -1,13 +1,53 @@
 import './style.css'
 
-import imgUrl from '../public/vite.svg'
-document.getElementById('vite.svg').src = imgUrl;
-const main = document.querySelector('main');
-window.addEventListener('backgorun', (e) => {
-  console.log(location.bash.slide(1));
-});
-const llamado = async (exit) => {
-  console.log("llamado");
-  const precicion = await fetch('ruta')
-}
-console.log(llamado);
+console.clear();
+
+
+
+
+const categorias = [
+  {
+    nombre: 'Rosas', descripcion: 'Flores rojas, perfectas para el amor'
+  },
+  {
+    nombre: 'Tulipanes', descripcion: 'Elegancia y colores vibrantes'
+  },
+  {
+    nombre: 'Orquídeas', descripcion: 'Delicadas y exóticas'
+  },
+  {
+    nombre: 'Girasoles', descripcion: 'Brillantes como el sol'
+  },
+];
+
+
+// modificamos el dom  y agrgamos un evento esta va tener una funcion anonima que espera una promesa 
+// al momento de dar un clik 
+document.addEventListener('DOMContentLoaded', () => {
+  const enlazarCategorias = document.querySelectorAll('.menu-navegacion__link');// enlazamos  con el querySelectorall en este buscamos   con su respectiva clase
+  const contenedor = document.getElementById('.contenedor-categorias'); // creamos una costante  esta se encargar de insertar todo en el contenedor 
+  //lamamos la constante 
+  enlazarCategorias.forEach(link => {
+    if (link.textContent.includes('Categorias')) {
+      link.addEventListener('clik', (e) => {
+        e.preventDefault();
+        contenedor.innerHTML = '';
+      })
+      categorias.forEach(cat => {
+        const tarjeta = document.createElement('div');
+        tarjeta.classList.add('tarjeta-categoria');
+        //aca insiertamos con el InerHtml
+        tarjeta.innerHTML = `
+        <h3>${cat.nombre}</h3>
+        <p>${cat.descripcion}</p>
+        `
+        contenedor.appendChild(tarjeta);
+      })
+    }
+  })
+})
+
+
+window.addEventListener('hashchange', async () => {
+  const hash = location.hash.slice(1);
+})
